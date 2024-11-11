8e0799886cac3be9dbf855800b355eb4727971cb
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-11-11 23:59:34 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,6 @@ export default function Navbar(props) {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Link</a>
-          </li>
-        </ul>
         {/* <form className="d-flex" role="search">
           <input className="form-control me-2 mx-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit">Search</button>
